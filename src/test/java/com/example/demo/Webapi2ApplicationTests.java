bafcdcca29d1c872243f2fc91e69ee0e488fa694
Aki330 <ap_aki330@icloud.com>
2024-06-21 13:31:16 +0900

    初回のコミットです

---
+++ src/test/java/com/example/demo/Webapi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
